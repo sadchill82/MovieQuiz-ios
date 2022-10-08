7932a44424233dc0e7338b808c399deacff521ad
sadchill82 <nurmuhametoffislam@yandex.ru>
2022-10-08 16:10:43 +0300

    fixed ui test

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -17,9 +17,9 @@ class MovieQuizUITests: XCTestCase {
     
     func testYesButton() {
         let firstPoster = app.images["Poster"]
-        app.buttons["Yes"].tap()
+        app.buttons["Да"].tap()
         let secondPoster = app.images["Poster"]
-        let indexLabel = app.staticTexts["Index"]
+        let indexLabel = app.staticTexts["Вопрос:"]
         sleep(3)
         XCTAssertTrue(indexLabel.label == "2/10")
         XCTAssertFalse(firstPoster == secondPoster)
@@ -27,9 +27,9 @@ class MovieQuizUITests: XCTestCase {
     
     func testNoButton() {
         let firstPoster = app.images["Poster"]
-        app.buttons["No"].tap()
+        app.buttons["Нет"].tap()
         let secondPoster = app.images["Poster"]
-        let indexLabel = app.staticTexts["Index"]
+        let indexLabel = app.staticTexts["Вопрос:"]
         sleep(3)
         XCTAssertTrue(indexLabel.label == "2/10")
         XCTAssertFalse(firstPoster == secondPoster)
@@ -38,10 +38,10 @@ class MovieQuizUITests: XCTestCase {
     func testAlertExist() {
         sleep(4)
         for _ in 0..<10 {
-            app.buttons["Yes"].tap()
+            app.buttons["Да"].tap()
             sleep(2)
         }
-        let indexLabel = app.staticTexts["Index"]
+        let indexLabel = app.staticTexts["Вопрос:"]
         XCTAssertTrue(indexLabel.label == "10/10")
         let resultAlert = app.alerts["result_alert"]
         XCTAssertTrue(resultAlert.exists)
@@ -53,7 +53,7 @@ class MovieQuizUITests: XCTestCase {
         sleep(5)
         var i = 0
         while i < 10 {
-            app.buttons["Yes"].tap()
+            app.buttons["Да"].tap()
             sleep(5)
             i += 1
         }
